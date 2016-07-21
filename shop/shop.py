d63a2140004193caba85490e1d96caa5f448ae51
Andrew Wansley <andrew.wansley@gmail.com>
2016-07-20 23:42:53 -0700

    Define behavior more carefully with a test suite

--- shop/shop.py
+++ shop/shop.py
@@ -1,21 +1,28 @@
 import logging
 import itertools
 
-class SymbolMeta(type):
-    def __getattr__(cls, name):
-        if name.startswith("__"):
-            super(SymbolMeta, type).__getattr__(name)
-        return cls(name=name)
 
-def flatmap(func, iterable):
-    return list(itertools.chain.from_iterable(map(func, iterable)))
+class SymbolMeta(type):
+
+  def __getattr__(cls, name):
+    if name.startswith("__"):
+      super(SymbolMeta, type).__getattr__(name)
+    return cls(name=name)
+
+  def __getitem__(cls, idx):
+    return cls(name=idx)
 
 
 class KResult(dict):
   pass
 
 
+def flatmap(func, iterable):
+  return list(itertools.chain.from_iterable(map(func, iterable)))
+
+
 class DictCombiner(object):
+
   def __repr__(self):
     return "(%s + %s)" % (self.left, self.right)
 
@@ -27,9 +34,9 @@ class DictCombiner(object):
     def merge(child, self_result):
       child_result = child(obj)
       if isinstance(child_result, KResult):
-        return dict(self_result.items() + child_result.items())
+        return KResult(self_result.items() + child_result.items())
       else:
-        return dict(self_result.items() + [(child._path(), child_result)])
+        return KResult(self_result.items() + [(child._path(), child_result)])
 
     self_result = KResult()
     self_result = merge(self.left, self_result)
@@ -39,66 +46,76 @@ class DictCombiner(object):
   def __add__(self, other):
     return DictCombiner(self, other)
 
-  def __mul__(self, other):
-    return map(self, other)
-
-  def __pow__(self, other):
-    return flatmap(self, other)
-
 
 class k(object):
-    __metaclass__ = SymbolMeta
+  __metaclass__ = SymbolMeta
 
-    def __repr__(self):
-        chain = self._chain()
-        names = [str(k_.name) for k_ in chain]
-        return "k<%s>" % ".".join(names)
+  def __repr__(self):
+    chain = self._chain()
+    names = [str(k_.name) for k_ in chain]
+    return "k<%s>" % ".".join(names)
 
-    def __init__(self, name=None, prev=None):
-        self.name = name
-        self.prev = prev
+  def __init__(self, name=None, prev=None):
+    self.name = name
+    self.prev = prev
+    self.flatten = None
+    self.default = None
 
-    def __getattr__(self, name):
-        return k(name=name, prev=self)
+  def __getattr__(self, name):
+    return k(name=name, prev=self)
 
-    def __getitem__(self, idx):
-        return k(name=idx, prev=self)
+  def __getitem__(self, idx):
+    return k(name=idx, prev=self)
 
-    def __call__(self, obj):
-        if self.prev:
-            res = self.prev(obj)
-        else:
-            res = obj
-        logging.debug(str(self), res)
-        if res is not None:
-            if self.name == "_":
-              return res
-            attr_or_none = getattr(res, str(self.name), None)
-            if attr_or_none is None and hasattr(res, '__getitem__'):
-                try:
-                    attr_or_none = res.__getitem__(self.name)
-                except (IndexError, KeyError):
-                    pass
-            return attr_or_none
-        else:
-            return res
+  def try_hard_to_get(self, obj):
+    if self.name == "_":
+      return obj
+    attr_or_none = getattr(obj, str(self.name), self.default)
+    if attr_or_none is self.default and hasattr(obj, '__getitem__'):
+      try:
+        attr_or_none = obj.__getitem__(self.name)
+      except (IndexError, KeyError):
+        return self.default
+      except TypeError:
+        if isinstance(obj, list):
+          if self.flatten is True:
+            return flatmap(self.try_hard_to_get, obj)
+          else:
+            return map(self.try_hard_to_get, obj)
+    return attr_or_none
 
-    def _chain(self):
-        if self.prev:
-            return self.prev._chain() + [self]
-        else:
-            return [self]
+  def __call__(self, *args, **kwargs):
+    flatten = kwargs.pop('flatten', None)
+    if self.flatten is None:
+      self.flatten = flatten
 
-    def _path(self):
-        chain = self._chain()
-        names = [str(k_.name) for k_ in chain]
-        return "%s" % "_".join(names)
+    default = kwargs.pop('default', None)
+    if self.default is None:
+      self.default = default
 
-    def __add__(self, other):
-        return DictCombiner(self, other)
+    if len(args) == 0:
+      return self
+    elif len(args) == 1:
+      obj = args[0]
+      if self.prev:
+        res = self.prev(obj)
+      else:
+        res = obj
+      if res is not None:
+        return self.try_hard_to_get(res)
+      else:
+        return res
 
-    def __mul__(self, other):
-        return map(self, other)
+  def _chain(self):
+    if self.prev:
+      return self.prev._chain() + [self]
+    else:
+      return [self]
 
-    def __pow__(self, other):
-        return flatmap(self, other)
+  def _path(self):
+    chain = self._chain()
+    names = [str(k_.name) for k_ in chain]
+    return "%s" % "_".join(names)
+
+  def __add__(self, other):
+    return DictCombiner(self, other)
---
+++ test_shop.py
@@ -0,0 +1,89 @@
+from shop import k
+
+
+class Obj(dict):
+
+  def __getattr__(self, attr):
+    try:
+      return self[attr]
+    except KeyError:
+      raise AttributeError
+
+  def __setattr__(self, attr, value):
+    self[attr] = value
+
+
+def test_attribute_access():
+  o = Obj()
+  o.foo = 'bar'
+  assert k.foo(o) == 'bar'
+  assert k.baz(o) == None
+
+
+def test_default():
+  o = Obj()
+  o.foo = 'bar'
+  assert k.foo(o, default=True) == 'bar'
+  assert k.bar(o, default=True) is True
+
+
+def test_nested_access():
+  o, c = Obj(), Obj()
+  c.foo = 'bar'
+  o.child = c
+  assert k.child.foo(o) == 'bar'
+  assert k.child.bar(o) == None
+  assert k.foo.bar(o) == None
+
+
+def test_list_indexing():
+  o = Obj()
+  a, b = Obj(), Obj()
+  ls = [a, b]
+  o.ls = ls
+  assert k[0](ls) == a
+  assert k.ls[0](o) == a
+
+
+def test_self():
+  o = Obj()
+  assert k._(o) == o
+
+
+def test_addition():
+  o = Obj()
+  o.foo = 'bar'
+  o.bar = 'baz'
+  o.baz = 'quux'
+  assert (k.foo + k.bar +
+          k.baz)(o) == {'foo': 'bar', 'bar': 'baz', 'baz': 'quux'}
+
+
+def test_addition_and_nesting():
+  o, c = Obj(), Obj()
+  c.foo = 'bar'
+  o.foo = 'bar'
+  o.child = c
+  assert (k.foo + k.child.foo)(o) == {'foo': 'bar', 'child_foo': 'bar'}
+
+
+def test_nested_list():
+  o, a, b = Obj(), Obj(), Obj()
+  a.foo = 'bar'
+  b.foo = 'baz'
+  o.children = [a, b]
+  assert k.children.foo(o) == ['bar', 'baz']
+
+
+def test_flatten():
+  o, a, b = Obj(), Obj(), Obj()
+  w, x, y, z = Obj(), Obj(), Obj(), Obj()
+  w.foo = 'bar'
+  x.foo = 'bar'
+  y.foo = 'bar'
+  z.foo = 'bar'
+  a.children = [w, x]
+  b.children = [y, z]
+  o.children = [a, b]
+  assert k.children.children.foo(o) == [['bar', 'bar'], ['bar', 'bar']]
+  assert k.children.children(flatten=True).foo(o) == ['bar', 'bar', 'bar', 'bar']
